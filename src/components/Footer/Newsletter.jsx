e586e16dba7d93efe29daa6e19752a4ffd3be44f
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-07-19 19:13:29 +0100

    find us and newsletter done

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="Newsletter" />
+      <h2 className='headtext_comorant'>Subscribe to Our Newsletter</h2>
+      <p className='p__opensans'>And Never Miss Latest Updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type='email' placeholder='Enter your email adress' />
+      <button className='custom__button' type='button'>Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {FiFacebook, FiTwiiter, FiInstagram} from "react-icons/fi";
+import Newsletter from "../../components/Footer/Newsletter"
+import FooterOverlay from "../../components/Footer/FooterOverlay"
 
 import './Footer.css';
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
-
-export default Footer;
+export default function Footer() {
+  return (
+    <div className='app__footer section__padding'>
+      {/* <FooterOverlay /> */}
+      <Newsletter />
+    </div>
+  )
+}
