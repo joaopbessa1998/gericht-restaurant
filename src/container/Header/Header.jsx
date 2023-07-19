377342893221b24998e266fcd25c777959ed7a86
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-07-19 11:47:16 +0100

    header done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { images } from "../../constants/images"
+import { SubHeading } from "../../components"
 import './Header.css';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
-
-export default Header;
+export default function Header() {
+  return (
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading
+          title="Chase the new flavor"
+        />
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      </div>
+      <div className='app__wrapper_img'></div>
+    </div>
+  )
+}
